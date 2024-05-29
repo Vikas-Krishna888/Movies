264466dbc91c9bb78ca7d528f8898b04134e5217
V <vikas.b.v@capgemini.com>
2024-05-29 15:27:18 +0530

    search and watchlist commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import "./App.css";
 import "./styles.css";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/WatchList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const toggleWatchlist = (movieId) => {
+    // setting previous state and (remove if its included else add if its not included)
+    setWatchlist((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
+
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <MoviesGrid />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            />
+            <Route
+              path="/watchlist"
+              element={
+                <WatchList
+                  watchlist={watchlist}
+                  movies={movies}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            />
+          </Routes>
+        </Router>
+        {/* <MoviesGrid /> */}
       </div>
       <Footer />
     </div>
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,27 +1,117 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
+  // const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
+
+  // useEffect(() => {
+  //   fetch("movies.json")
+  //     .then((response) => response.json())
+  //     .then((data) => setMovies(data));
+  // }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  // search results filter
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  // genres filter
+  const matchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
+    );
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  // ratings filter
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
+      case "Good":
+        return movie.rating >= 7.5;
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 7.5;
+      case "Bad":
+        return movie.rating < 5;
+      default:
+        return false;
+    }
+  };
+
+  // combined filtering
+  const filterMovieResults = movies.filter(
+    (movie) =>
+      matchesRating(movie, rating) &&
+      matchesGenre(movie, genre) &&
+      matchesSearchTerm(movie, searchTerm)
+  );
 
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <div className="movie-card" key="movie.id">
-          <img src={`images/${movie.image}`} alt={movie.title} />
-          <div className="movie-card-info">
-            <h3 className="movie-card-title">{movie.title}</h3>
-            <h3 className="movie-card-genre">{movie.genre}</h3>
-            <h3 className="movie-card-rating">{movie.rating}</h3>
-          </div>
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
         </div>
-      ))}
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+      <div className="movies-grid">
+        {filterMovieResults.map((movie) => (
+          // movies.map((movie) => ( // without searched data
+          <MovieCard
+            movie={movie}
+            isWatchlisted={watchlist.includes(movie.id)}
+            toggleWatchlist={toggleWatchlist}
+            key={movie.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
